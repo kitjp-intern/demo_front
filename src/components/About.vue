a68c3b03eb8874b2bd9e6d1f91e3184fb2b4ae7c
白濱淳也 <juuuunyaaaa070120@gmail.com>
2020-12-07 10:40:24 +0900

    mv alot

---
+++ src/components/About.vue
@@ -0,0 +1,38 @@
+<!-- Home data -->
+<template>
+  <v-container >
+    <v-row justify="center">
+    <v-card width="800px">
+    <v-row justify="center">
+    <h1>Haystack Q&Aの使い方</h1>
+    </v-row>
+    <v-row justify="center">
+      <ol>
+        <li>メニューの<v-icon>{{'mdi-text-box'}}</v-icon>Choose dataを選んでデータを選択してください</li>
+        <li>選択後、<v-btn x-small rounded color='primary'>データを決定</v-btn>を押してください</li>
+        <li>質問文を記入後<v-btn x-small rounded  color='primary'>Send</v-btn></li>
+        <li>答えを確認してください。詳細を知りたい場合<v-btn x-small rounded color='primary'>詳細</v-btn>をクリック</li>
+        <li>他の回答を確認したい場合はカードをクリックしてください</li>
+      </ol>
+    </v-row>
+  </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+
+<style>
+#home{
+  text-align: center;
+}
+
+</style>
