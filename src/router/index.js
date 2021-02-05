e6ab81a859ded54c7222f55e91480cc90dc95986
shira07tech01 <srhmjny0701@gmail.com>
2021-02-05 17:22:22 +0900

    add child name

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import HaystackQA from '../views/HaystackQA/parent.vue'
+import HaystackQA from '../views/HaystackQA/parent'
 import Question from  '../views/HaystackQA/Question'
 import Answer from '../views/HaystackQA/Answer'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    component: Home
-  },
-  {
-    path: '/HaystackQA',
+  {path: '/', component: Home},
+  { path: '/HaystackQA',
     component: HaystackQA,
     childlen:[
     {
       path: '',
-      component: Question
+      components: {
+        child:Question
+      }
     },
     {
-      path: '/answer',
-      component: Answer
-    }]
+      path: 'answer',
+      components: {
+        child:Answer
+    }}]
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  //base: process.env.BASE_URL,
+  base: process.env.BASE_URL,
   routes,
-  //linkActiveClass: 'active',
+  linkActiveClass: 'active',
 })
 
 export default router
--- src/views/HaystackQA/parent.vue
+++ src/views/HaystackQA/parent.vue
@@ -7,11 +7,11 @@
       </v-col>
       <v-col cols="12" sm="12" md="12" lg="6">
         <ul>
-          <li><router-link to="/HaystackQA">Question</router-link></li>
+          <li><router-link to="/HaystackQA/">Question</router-link></li>
           <li><router-link to="/HaystackQA/answer">answer</router-link></li>
         </ul>
         <div class="child-page">
-          <router-view/>
+          <router-view name="child"></router-view>
           <p>
             ああああああああああああああああ
           </p>
