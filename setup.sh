c2eada7553ad9e2ed4eee3fa09569a102baf855f
shira07tech01 <srhmjny0701@gmail.com>
2021-02-05 10:27:36 +0900

    add comment

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+#nodejs npm install and n pacage install
 apt install -y nodejs npm
 sudo npm install n -g
 
+#purge nodejs npm
 sudo n stable
 sudo apt purge -y nodejs npm
 exec $SHELL -l
+
+#http-server install
+npm install http-server
